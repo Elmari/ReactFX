23e465581c7fb83074ab9a398bc4d7b33baacfe6
Art Pinch <leonardo906@mail.ru>
2021-02-15 21:03:48 +0300

    Update to use Java 9+ modularity

---
+++ reactfx/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module reactfx {
+
+    requires javafx.graphics;
+    requires javafx.controls;
+
+    exports org.reactfx;
+    exports org.reactfx.collection;
+    exports org.reactfx.inhibeans;
+    exports org.reactfx.inhibeans.collection;
+    exports org.reactfx.inhibeans.value;
+    exports org.reactfx.inhibeans.binding;
+    exports org.reactfx.inhibeans.property;
+    exports org.reactfx.util;
+    exports org.reactfx.value;
+
+}
